1e65a96538da83971214cc0e90252ee5ab246aae
gmarshall33 <46799857+gmarshall33@users.noreply.github.com>
2019-04-23 13:36:05 -0400

    moving functions into appropriate modules

--- modules/module3.py
+++ modules/module3.py
@@ -1,2 +1,111 @@
 # This is where bonds between atoms will be recognized using simple hough type functions
 
+import numpy as np
+
+
+def detect_lines(img, center, threshold):
+    # given a 1) inverse binary image, where the lines of interest are white and background is black
+    # 2) an index pair corresponding to the center of an interest region on the image,
+    # and 3) the max length of the line you want to detect
+    # >> find possible lines originating from that center
+    y, x = center[0], center[1]
+    theta = np.linspace(0, 2*np.pi, num=360)  # consider polar  coordinates x = r*cos(theta) y = r*sin(theta)
+    theta = theta.tolist()
+    out = []  # list of the number of votes each angle got
+    points = []  # points corresponding to votes for each angle (mostly for visualization purposes)
+    for o in theta:
+        r = 1  # radius, initially one for every angle
+        votes = 0
+        line_pts = []  # points for the current line
+        while r <= threshold:  # r will increase every iteration until it reaches the max length which is threshold
+            m = int(round(r*np.sin(o)))
+            n = int(round(r*np.cos(o)))  # have to add n,m to x,y to adjust for location of center
+            try:
+                if img[m+y, n+x] > 0:  # if the current point on the image is not black...
+                    votes += 1
+                    line_pts.append([m+y, n+x])
+            except IndexError:
+                pass
+            r += 1
+        points.append(line_pts)  # add current line points to the master list of points
+        out.append((votes, o))
+        # out is a list of tuples containing (votes, corresponding angle)
+    return out, points
+    # points are returned mostly for plotting purposes
+
+
+def check_direction(center_cluster, theta, other_cluster, max_percent_error=1):
+    # Given a center node, strong direction angle, and another node for comparison...
+    # >>> determine if the center is in the given direction from the node (returns boolean)
+    cent = center_cluster.center
+    center = other_cluster.center
+    m, n = cent[0], cent[1]
+    d = (((m-center[0])**2) + ((n - center[1])**2))**0.5  # distance formula
+    if d == 0:
+        return None
+        # if we are checking a center with itself return none
+    # thus begins the trigonometry and unit circle stuff:::
+    psi = np.arcsin((center[0]-m) / d)
+    if (theta >= 0) and (theta <= np.pi/2):
+        # in quadrant 1 :  psi = theta
+        if percent_error(psi, theta) < max_percent_error:
+            return True
+    elif theta >= (np.pi/2) and (theta <= 1.5 * np.pi):
+        # in quadrant 2 and 3 :  psi = (pi)-theta
+        if percent_error(psi, (np.pi - theta)) < max_percent_error:
+            return True
+    elif (theta <= 1.5 * np.pi) and (theta <= 2*np.pi):
+        # in quadrant 4 :  pis = theta - 2(pi)
+        if percent_error(psi, (theta - (2*np.pi))) < max_percent_error:
+            return True
+    else:
+        return False
+# TODO there are bugs in this function that mess up the angles, also this isn't sufficient criteria to connect nodes
+
+def percent_error(x, y):  # helper function
+    if y < .1:
+        x += np.pi*2
+        y += np.pi*2
+    return (abs(x-y)/y)*100
+
+
+def calc_dist(ind):
+    # input a list of centers, will generate a distance matrix for reference when 'sorting'
+    (m, n) = np.shape(ind)
+    distance_matrix = np.zeros((m, m))
+    for c in range(len(ind)):
+        for p in range(len(ind)):
+            distance_matrix[c, p] = (((ind[c][1] - ind[p][1]) ** 2) + ((ind[c][0] - ind[p][0]) ** 2)) ** .5
+    return distance_matrix
+    # TODO optimize... upper and lower triangulars are mirrored, diagonal is zeros
+
+
+def strong_directions(vote_angle, thresh=.65):
+    # given list of tuples (votes, angles) >>> return list of angles with many votes
+    # SORT ANGLES BY MOST VOTES
+    vote_angle.sort(reverse=True)  # list of tuples (votes, angle) from greatest to smallest votes
+    allVotes = [v[0] for v in vote_angle]
+    maxVote = max(allVotes)
+    allAng = [d[1] for d in vote_angle]
+    strongAng = [allAng[i] for i in range(len(allVotes)) if allVotes[i] >= (maxVote*thresh)]  # list comprehension flex
+    return strongAng  # a list of the strongest angles ( <=(thresh)% of the  maximum votes) default 65%
+
+
+def nodes_by_dist(distance_matrix, center_index):
+    # given a square distance matrix and index for the center node
+    # >>> return indices of nodes sorted by distance to the center node
+    d = np.argsort(distance_matrix[center_index, :])
+    d = d.tolist()
+    d = map(int, d)
+    return d  # list of indices corresponding to molecule[index] , sorted by distance closest to farthest
+
+
+def bond_clusters(molecule, ind1, ind2):
+    molecule[ind1].connections.add(ind2)
+    molecule[ind2].connections.add(ind1)
+    # not worried about duplicated because connections is a set. [unordered unique entries, iterable]
+
+
+
+
+
--- modules/module5.py
+++ modules/module5.py
@@ -1,2 +1,111 @@
 # This module will contain the algorithm to convert the completed molecular data structure to SMILES string or anther
 # standardized format.
+
+# also include some visualization functions since haven't programed SMILES interpreter yet
+
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import collections as mc
+import random
+import numpy as np
+
+
+def vis_clusters(img, dst, clusters):
+    # VISUALIZATION INFO for corner detection and clustering results
+    cv2.imshow('orginal', img)
+
+    simg = img
+    simg[dst > 0.001 * dst.max()] = [0, 0, 255]
+    cv2.imshow('corners detected', simg)
+
+    clustimg = img
+    for i in range(len(clusters)):
+        yeet = round(random.uniform(0, 1) * 255)
+        for w in clusters[i]:
+            clustimg[w[0], w[1], :] = [255 / (i + 1) * 2, 255 - yeet, yeet]
+            cv2.imshow('w', clustimg)
+
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+    return clustimg
+
+
+def vclust(img, dst, clusters):
+    # copy of vis_clusters but without the imshow() windows for use in vis_molecule
+    simg = img
+    simg[dst > 0.001 * dst.max()] = [0, 0, 255]
+    clustimg = img
+    for i in range(len(clusters)):
+        yeet = round(random.uniform(0, 1) * 255)
+        for w in clusters[i]:
+            clustimg[w[0], w[1], :] = [255 / (i + 1) * 2, 255 - yeet, yeet]
+    return clustimg
+
+
+def vis_centers(img, centers):
+    zimg = img
+    for cent in centers:
+        zimg[cent[0]-3:cent[0]+3, cent[1]-3:cent[1]+3, :] = [0, 255, 0]
+    cv2.imshow('centers', zimg)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+    # colors the centers green on an img
+    return zimg
+
+
+def draw_lines(img, point1, angle):
+    fig, ax = plt.subplots()
+    ax.imshow(img)  # extent = [0, n, 0, m])
+    endm = point1[0] + (100 * np.sin(angle))
+    endn = point1[1] + (100 * np.cos(angle))
+
+    # collection = mc.LineCollection(segments)
+    # ax3.add_collection(collection)
+    ax.plot([point1[1], endn], [point1[0], endm])
+    plt.show()
+
+
+def vis_molecule(molecule, img_data, save_fig=False, filename='untitled.png'):
+    # PRODUCES PLOT WITH ORIGINAL IMAGE, CLUSTERS, AND CONNECTION GRAPH
+    # molecule list of cluster class, img_data dictionary, optional params for if you want to save the figure
+    segments = []
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 10))
+    fig.suptitle('Atoms and Bonds', fontsize=18)
+    ax1.imshow(img_data['original'])  # image
+    ax1.set_title('Original Image')
+    ax1.axis('off')
+
+    ax2.imshow(vclust(img_data['original'], img_data['dst'], [x.points for x in molecule]))  # clusters from vclust()
+    ax2.set_title('Clusters from Corner Detection')
+    ax2.axis('off')
+
+    for node in range(len(molecule)):  # circles for nodes
+        cent1 = molecule[node].center
+        c1 = (cent1[1], -1*cent1[0])
+        circle1 = plt.Circle((molecule[node].center[1], (molecule[node].center[0])*-1), 5, color='y')
+        plt.annotate(node, c1)  # add number labels
+        ax3.add_artist(circle1)
+
+        for ind in molecule[node].connections:  # line segments between nodes
+            cent2 = molecule[ind].center
+            c2 = (cent2[1], -1*cent2[0])
+            line = [c1, c2]
+            segments.append(line)
+
+    collection = mc.LineCollection(segments)
+    ax3.add_collection(collection)
+    ax3.set_title('Nodes and Connections')
+    ax3.autoscale()
+    ax3.margins(.5)
+    ax3.axis('off')
+    if save_fig:
+        plt.savefig(filename)
+    plt.show()
+
+
+def vis_vote_angle(vote_angle):
+    vote_angle.sort(reverse=True)  # list of tuples (votes, angle) from greatest to smallest votes
+    allVotes = [v[0] for v in vote_angle]
+    allAng = [d[1] for d in vote_angle]
+    plt.plot(allAng, allVotes, 'o')
+    plt.show()
